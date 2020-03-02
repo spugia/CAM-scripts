56b53e046de9d1f6da4b0baf24344b041a6332ff
Steven Pugia <stevenpugia@Stevens-MacBook-Pro.local>
2020-03-02 11:18:38 -0500

    commenting the square pocket code

--- square_pocket.m
+++ square_pocket.m
@@ -1,3 +1,64 @@
+%{
+ PURPOSE: This function generates the g-code for machining a square shaped pocket.
+          The geometry consists of an outer square and an optional inner square with 
+          the cutting space in between them.
+
+ y
+ ^  
+ |  TOP VIEW
+ |
+ * ----> x
+Z
+
+ ---------------------
+ |                   |
+ |      --------     |
+ |      |      |     |
+ |      |      | Lyi | Lyo
+ |      |      |     |
+ |      --------     |
+ |        Lxi        |
+ ---------------------
+          Lxo
+
+ INPUTS:
+
+ - file: the file that the g-code should be writen to.
+         note: the file will not be closed at the end of the function.
+
+ - N:    the current line number to start counting from. If the file already 
+         contains 50 g-code then pass 'N' as '50' so the new code will be ordered
+         correctly.
+
+ - b:    the diameter of the cutting bit in millimeters.
+
+ - Fd:   the plunging feed rate for the mill.
+         note: the maximum that the minimill can do is 150 mm/min.
+
+ - Fl:   the translational feed rate for the mill.
+         note: the maximum that the minimill can do is 150 mm/min.
+
+ - P0:   the origin point (relative to machine zero) that the square will be centered on.
+
+ - Lxi:  the length of the inner square in the 'X' direction in millimeters.
+ - Lxo:  the length of the outer square in the 'X' direction in millimeters.
+ - Lyi:  the length of the inner square in the 'Y' direction in millimeters.
+ - Lyo:  the length of the outer square in the 'Y' direction in millimeters.
+ - h:    the depth of the pocket in millimeters
+
+ - zdiv: the depth that the mill will cut off in the 'Z' direction with each 
+         layer in millimeters.
+
+ - addheader: a flag to indicate whether the header code should be added to the file.
+              Do this if this operation is the first in a sequence.
+
+ - startatorigin: a flag to indicate whether the machine should move to the origin before
+                  starting the machining operation. In general it is a good idea to do this.
+
+ - addfooter: a flag to indicate whether the footer code should be added to the file.
+              Do this if the operation is the last in a sequence.
+%}
+
 function N = square_pocket(file, N, b, Fd, Fl, P0, Lxi, Lyi, Lxo, Lyo, h, zdiv, addheader, startatorigin, addfooter)
 
 	if (Lxi < 0)

8da399fff79dd83911fb7a3579ce8fddc85091f9
Steven Pugia <spugia@purdue.edu>
2020-09-25 14:27:15 -0400

    fixed bug in drill_holes

--- drill_holes.m
+++ drill_holes.m
@@ -47,11 +47,11 @@ function [N] = drill_holes(file, N, Fd, holes, Z0, h, addheader, startatorigin, 
         fprintf(file, 'N%d G00 X0 Y0\n', N); N = N + 1;
     end
 
-    for h = [1 : 1 : size(holes, 1)]
+    for n = [1 : 1 : size(holes, 1)]
 
     	fprintf(file, 'N%d G00 Z1\n', N); N = N + 1;
         fprintf(file, 'N%d G00 X0 Y0\n', N); N = N + 1;
-        fprintf(file, 'N%d G00 X%.4f Y%.4f\n', N, holes(h, 1), holes(h, 2)); N = N + 1;
+        fprintf(file, 'N%d G00 X%.4f Y%.4f\n', N, holes(n, 1), holes(n, 2)); N = N + 1;
         fprintf(file, 'N%d G00 Z0\n', N); N = N + 1;
         fprintf(file, 'N%d G01 Z%.4f F%.2f\n', N, Z0-h, Fd); N = N + 1;
     end
